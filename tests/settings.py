053437785f67d3056c50c9937581e93310c5b8e0
Benjamin PIERRE <contact@benjamin-pierre.dev>
2026-03-18 09:50:37 +0100

    FIX: Broken tests
    Signed-off-by: Benjamin PIERRE <contact@benjamin-pierre.dev>

--- tests/settings.py
+++ tests/settings.py
@@ -3,3 +3,4 @@ DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3"}}
 ROOT_URLCONF = "tests.urls"
 ALTCHA_HMAC_KEY = "altcha-insecure-hmac-0123456789abcdef"
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"}}
+STATIC_URL = "/static/"
